403fbfd982e6b2f788dc1007e27dd28a6faa00a6
justinmorales1 <justin@justins-mbp.lan>
2021-02-03 21:01:35 -0600

    Using using lodash and uniq to reduce the number of xhr requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,19 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
 
+export const fetchPostsAndUsers = () => async (dispatch, getState )=> {
+
+    await dispatch(fetchPosts());
+
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+
+};
+
+
 export const fetchPosts = () => async dispatch => {
     const response =  await jsonPlaceholder.get('/posts');
 
@@ -11,10 +24,10 @@ export const fetchPosts = () => async dispatch => {
     })
 };
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
 
 
-const _fetchUser = _.memoize(async(id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
+
     const response =  await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({
@@ -22,4 +35,17 @@ const _fetchUser = _.memoize(async(id, dispatch) => {
         payload: response.data
     })
 
-});
+};
+
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+//
+//
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//     const response =  await jsonPlaceholder.get(`/users/${id}`);
+//
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+//
+// });
