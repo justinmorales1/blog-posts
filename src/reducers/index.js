2f787acc016adace3c6d437ce22cf4e7d0193249
justinmorales1 <justin@justins-mbp.lan>
2021-02-03 18:08:04 -0600

    Adding new jsxs and reducers to handle users and posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import postsReducer from './postsReducer'
+import userReducer from './usersReducer'
 
 
 
 
-
-export default combineReducers({});
+export default combineReducers({posts : postsReducer, users: userReducer});
